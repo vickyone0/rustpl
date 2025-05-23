eca5b6d5edec2e3f4ef87e251f4f85e0d26d41ac
vickyone0 <vigneshs0.618@gmail.com>
2025-05-23 02:24:17 -0400

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-impl Rectangle {
-    fn area(&self) -> u32 {
-    self.width * self.height
-    }
 
-    fn width(&self) -> u32 {
-        self.width
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+#[derive(Debug)]
+enum IpAddr {
+ V4(u8, u8, u8, u8),
+ V6(String),
 }
 
-
-fn main(){
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 40,
-        height: 60,
-    };
-    let v = Rectangle::area(&self.rect1);
-
-    print!("Area of the rectangle is {}, {}/n is can hold is avalible ? {}", rect1.width(),rect1.area(), rect2.can_hold(&rect1));
+fn main() {
+    // Example usage of the IpAddr struct
+let home = IpAddr::V4(127, 0, 0, 1);
+let loopback = IpAddr::V6(String::from("::1"));
+       
+       
+    println!("Home IP: {:?} ", home) ;
+    println!("Loopback IP: {:?}", loopback );
 }
