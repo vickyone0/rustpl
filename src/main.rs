9d5fe8f5bc930cbe5a0d0379257e247e41c415ed
vickyone0 <vigneshs0.618@gmail.com>
2025-05-28 09:59:56 -0400

    unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::thread;
 #[derive(Debug)]
 enum IpAddr {
  V4(u8, u8, u8, u8),
@@ -13,4 +13,17 @@ let loopback = IpAddr::V6(String::from("::1"));
        
     println!("Home IP: {:?} ", home) ;
     println!("Loopback IP: {:?}", loopback );
+
+
+    let mut data = vec![1, 2, 3, 4, 5];
+
+    thread::spawn(move || {
+        println!("{:?}", data);
+    }).join().unwrap();
+
+
+    let value_1 = Some(5);
+    let value_2 = None;
+    let value_3 = value_1.unwrap();
+    let value_4 = value_2.unwrap();
 }
