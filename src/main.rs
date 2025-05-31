2eda2a3ba6dde8f6dbd5c5075e8c7c277add6cdf
vickyone0 <vigneshs0.618@gmail.com>
2025-05-31 07:02:13 -0400

    struct

--- src/main.rs
+++ src/main.rs
@@ -143,8 +143,16 @@ let result;
 {
 let string2 = String::from("World");
 result = longest(string1.as_str(), string2.as_str());
-}
+
 println!("The longest string is: {}", result);
+}
+
+let novel = String ::from("Call me Ishmael. Some years ago...");
+let first_sentence = novel.split('.').next().expect("Could not find a sentence.");
+let i = ImportantExcerpt {
+    part: first_sentence,
+};
+println!("Important Excerpt: {}", i.part);
 }
 
 
@@ -177,4 +185,8 @@ fn longest<'a> (x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
+
+struct  ImportantExcerpt<'a> {
+    part: &'a str,
 }
